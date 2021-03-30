0d0a63d5fbc3d37b87d0bb3a4b626ac424783385
Michael <michael.adragna@gmail.com>
2021-03-30 16:23:20 +0200

    implement decode method

--- morse.py
+++ morse.py
@@ -1,5 +1,13 @@
 # morse.py
 # pylint: disable=missing-docstring
 
+def decode_one_letter(letter):
+    morse_dict = {".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E", "..-.": "F", "--.": "G", "....": "H",
+                  "..": "I", ".---": "J", "-.-": "K", ".-..": "L", "--": "M", "-.": "N", "---": "O", ".--.": "P",
+                  "--.-": "Q", ".-.": "R", "...": "S", "-": "T", "..-": "U", "...-": "V", ".--": "W", "-..-": "X",
+                  "-.--": "Y", "--..": "Z"}
+    return morse_dict.get(letter, "")
+
+
 def decode(message):
-    pass # TODO: implement the behavior!
+    return ''.join(list(map(decode_one_letter, message.split(' '))))
